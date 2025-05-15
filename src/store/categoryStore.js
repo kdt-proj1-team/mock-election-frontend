a6882f44307092f29afdef74735f42a1ae12c5c2
PJH <ksoark0109@naver.com>
2025-05-15 15:30:12 +0900

    게시판 관리 기능 구현

--- src/store/categoryStore.js
+++ src/store/categoryStore.js
@@ -8,7 +8,7 @@ const useCategoryStore = create((set) => ({
     // 카테고리 목록 API 호출 후 저장
     fetchCategories: async () => {
       try {
-        const data = await categoryAPI.getCategories();
+        const data = await categoryAPI.getCategoriesByIsActive();
         set({ categories: data });
       } catch (error) {
         console.error("카테고리 조회 실패:", error);
